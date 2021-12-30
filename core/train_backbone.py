5f1f7764a2955db88c94bf5f3b16b67b3343d635
leonardozcm <597714572@qq.com>
2021-12-31 01:13:43 +0800

    test metric

--- core/train_backbone.py
+++ core/train_backbone.py
@@ -4,48 +4,52 @@
 import logging
 import os
 import torch
+from core.test_backbone import test_backbone
 import utils.data_loaders
 import utils.helpers
 from datetime import datetime
 from tqdm import tqdm
 from time import time
 from tensorboardX import SummaryWriter
-from core.test_pcn import test_net
 from utils.average_meter import AverageMeter
 from torch.optim.lr_scheduler import StepLR
 from utils.schedular import GradualWarmupScheduler
-from utils.loss_utils import get_loss
+from utils.loss_utils import chamfer_sqrt
 from models.pcn import AutoEncoder
+from models.utils import fps_subsample
 
 
-def train_net(cfg):
+def train_backbone(cfg):
     # Enable the inbuilt cudnn auto-tuner to find the best algorithm to use
     torch.backends.cudnn.benchmark = True
 
-    train_dataset_loader = utils.data_loaders.DATASET_LOADER_MAPPING[cfg.DATASET.TRAIN_DATASET](cfg)
-    test_dataset_loader = utils.data_loaders.DATASET_LOADER_MAPPING[cfg.DATASET.TEST_DATASET](cfg)
+    train_dataset_loader = utils.data_loaders.DATASET_LOADER_MAPPING[cfg.DATASET.TRAIN_DATASET](
+        cfg)
+    test_dataset_loader = utils.data_loaders.DATASET_LOADER_MAPPING[cfg.DATASET.TEST_DATASET](
+        cfg)
 
     train_data_loader = torch.utils.data.DataLoader(dataset=train_dataset_loader.get_dataset(
         utils.data_loaders.DatasetSubset.TRAIN),
-                                                    batch_size=cfg.TRAIN.BACKBONE_BATCH_SIZE,
-                                                    num_workers=cfg.CONST.NUM_WORKERS,
-                                                    collate_fn=utils.data_loaders.collate_fn,
-                                                    pin_memory=True,
-                                                    shuffle=True,
-                                                    drop_last=False)
+        batch_size=cfg.TRAIN.BACKBONE_BATCH_SIZE,
+        num_workers=cfg.CONST.NUM_WORKERS,
+        collate_fn=utils.data_loaders.collate_fn,
+        pin_memory=True,
+        shuffle=True,
+        drop_last=False)
     val_data_loader = torch.utils.data.DataLoader(dataset=test_dataset_loader.get_dataset(
         utils.data_loaders.DatasetSubset.TEST),
-                                                  batch_size=cfg.TRAIN.BACKBONE_BATCH_SIZE,
-                                                  num_workers=cfg.CONST.NUM_WORKERS//2,
-                                                  collate_fn=utils.data_loaders.collate_fn,
-                                                  pin_memory=True,
-                                                  shuffle=False)
+        batch_size=cfg.TRAIN.BACKBONE_BATCH_SIZE,
+        num_workers=cfg.CONST.NUM_WORKERS//2,
+        collate_fn=utils.data_loaders.collate_fn,
+        pin_memory=True,
+        shuffle=False)
 
     # Set up folders for logs and checkpoints
-    output_dir = os.path.join(cfg.DIR.OUT_PATH, '%s', datetime.now().isoformat())
+    output_dir = os.path.join(cfg.DIR.OUT_PATH, '%s', '%s',
+                              datetime.now().isoformat())
     print(output_dir)
-    cfg.DIR.CHECKPOINTS = output_dir % 'pcn_checkpoints'
-    cfg.DIR.LOGS = output_dir % 'pcn_logs'
+    cfg.DIR.CHECKPOINTS = output_dir % ('backbone', 'checkpoints')
+    cfg.DIR.LOGS = output_dir % ('backbone', 'logs')
     if not os.path.exists(cfg.DIR.CHECKPOINTS):
         os.makedirs(cfg.DIR.CHECKPOINTS)
 
@@ -59,11 +63,13 @@ def train_net(cfg):
 
     # Create the optimizers
     optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),
-                                       lr=1e-4,
-                                       weight_decay=1e-6)
+                                 lr=cfg.TRAIN.BACKBONE_LEARNING_RATE,
+                                 weight_decay=cfg.TRAIN.WEIGHT_DECAY,
+                                 betas=cfg.TRAIN.BETAS)
 
     # lr scheduler
-    lr_scheduler = StepLR(optimizer, step_size=20, gamma=0.7)
+    lr_scheduler = StepLR(
+        optimizer, step_size=20, gamma=0.7)
     init_epoch = 0
     best_metrics = float('inf')
     steps = 0
@@ -77,14 +83,15 @@ def train_net(cfg):
         model.load_state_dict(checkpoint['model'])
         optimizer.load_state_dict(checkpoint['optimizer'])
         lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
-        logging.info('Recover complete. Current epoch = #%d; best metrics = %s.' % (init_epoch, best_metrics))
+        logging.info('Recover complete. Current epoch = #%d; best metrics = %s.' % (
+            init_epoch, best_metrics))
     else:
         # no need 2 warmup
         lr_scheduler = GradualWarmupScheduler(optimizer, multiplier=1, total_epoch=cfg.TRAIN.WARMUP_STEPS,
-                                            after_scheduler=lr_scheduler)
+                                              after_scheduler=lr_scheduler)
 
     # Training/Testing the network
-    count=0
+    count = 0
     for epoch_idx in range(init_epoch + 1, cfg.TRAIN.N_EPOCHS + 1):
 
         epoch_start_time = time()
@@ -94,95 +101,110 @@ def train_net(cfg):
 
         model.train()
 
-        total_cd_pc = 0
-        total_cd_p1 = 0
-        total_cd_p2 = 0
-        total_cd_p3 = 0
-        total_partial = 0
+        total_cd_loss, iter_count = 0, 0
+        total_cd_coarse = 0
+        total_cd_fine = 0
 
         batch_end_time = time()
         n_batches = len(train_data_loader)
 
-        accumulation_steps = 8 # 8 * bs(8) = 64(bs in paper)
+        accumulation_steps = 2  # 2 * bs(16) = 32(bs in paper)
         with tqdm(train_data_loader) as t:
             for batch_idx, (taxonomy_ids, model_ids, data) in enumerate(t):
-                # count+=1
-                if count>2:
+
+                # Debug switch
+                # count += 1
+                if count > 2:
                     break
+
                 data_time.update(time() - batch_end_time)
                 for k, v in data.items():
                     data[k] = utils.helpers.var_or_cuda(v)
-                partial = data['partial_cloud']
+                # partial = data['partial_cloud']
                 gt = data['gtcloud']
 
-                pcds_pred = model(partial)
+                #  downsample gt to 2048
+                partial = fps_subsample(gt, 2048)
+                coarse_gt = fps_subsample(gt, 1024)
 
-                loss_total, losses = get_loss(pcds_pred, partial, gt, sqrt=True)
+                # preprocess transpose
+                partial = partial.permute(0, 2, 1)
 
-                loss_total=loss_total/accumulation_steps
-                loss_total.backward()
-                
+                v, y_coarse, y_detail = model(partial)
+
+                y_coarse = y_coarse.permute(0, 2, 1)
+                y_detail = y_detail.permute(0, 2, 1)
+
+                loss_coarse = chamfer_sqrt(coarse_gt, y_coarse)
+                loss_fine = chamfer_sqrt(gt, y_detail)
+                loss = loss_coarse + 0.1 * loss_fine
+                loss.backward()
+
                 if (batch_idx+1) % accumulation_steps == 0:
                     optimizer.step()
                     optimizer.zero_grad()
 
-                    cd_pc_item = losses[0].item() * 1e3
-                    total_cd_pc += cd_pc_item
-                    cd_p1_item = losses[1].item() * 1e3
-                    total_cd_p1 += cd_p1_item
-                    cd_p2_item = losses[2].item() * 1e3
-                    total_cd_p2 += cd_p2_item
-                    cd_p3_item = losses[3].item() * 1e3
-                    total_cd_p3 += cd_p3_item
-                    partial_item = losses[4].item() * 1e3
-                    total_partial += partial_item
+                    cd_coarse = loss_coarse.item() * 1e3
+                    total_cd_coarse += cd_coarse
+                    cd_fine = loss_fine.item() * 1e3
+                    total_cd_fine += cd_fine
+                    cd_total = loss.item() * 1e3
+                    total_cd_loss += cd_total
+
                     n_itr = (epoch_idx - 1) * n_batches + batch_idx
-                    train_writer.add_scalar('Loss/Batch/cd_pc', cd_pc_item, n_itr)
-                    train_writer.add_scalar('Loss/Batch/cd_p1', cd_p1_item, n_itr)
-                    train_writer.add_scalar('Loss/Batch/cd_p2', cd_p2_item, n_itr)
-                    train_writer.add_scalar('Loss/Batch/cd_p3', cd_p3_item, n_itr)
-                    train_writer.add_scalar('Loss/Batch/partial_matching', partial_item, n_itr)
+                    train_writer.add_scalar(
+                        'Loss/Batch/cd_coarse', cd_coarse, n_itr)
+                    train_writer.add_scalar(
+                        'Loss/Batch/cd_fine', cd_fine, n_itr)
+                    train_writer.add_scalar(
+                        'Loss/Batch/cd_total', cd_total, n_itr)
+
                     batch_time.update(time() - batch_end_time)
                     batch_end_time = time()
-                    t.set_description('[Epoch %d/%d][Batch %d/%d]' % (epoch_idx, cfg.TRAIN.N_EPOCHS, batch_idx + 1, n_batches))
-                    t.set_postfix(loss='%s' % ['%.4f' % l for l in [cd_pc_item, cd_p1_item, cd_p2_item, cd_p3_item, partial_item]])
+                    t.set_description('[Epoch %d/%d][Batch %d/%d]' %
+                                      (epoch_idx, cfg.TRAIN.N_EPOCHS, batch_idx + 1, n_batches))
+                    t.set_postfix(loss='%s' % ['%.4f' % l for l in [
+                        cd_coarse, cd_fine, cd_total]])
 
-                    if steps <= cfg.TRAIN.WARMUP_STEPS:
+                    if 'WEIGHTS' not in cfg.CONST and steps <= cfg.TRAIN.WARMUP_STEPS:
                         lr_scheduler.step()
                         steps += 1
 
-        avg_cdc = total_cd_pc / n_batches
-        avg_cd1 = total_cd_p1 / n_batches
-        avg_cd2 = total_cd_p2 / n_batches
-        avg_cd3 = total_cd_p3 / n_batches
-        avg_partial = total_partial / n_batches
+        avg_cdc = total_cd_coarse / n_batches
+        avg_cdf = total_cd_fine / n_batches
+        avg_cdt = total_cd_loss / n_batches
 
         lr_scheduler.step()
-        print('epoch: ', epoch_idx, 'optimizer: ', optimizer.param_groups[0]['lr'])
+        print('epoch: ', epoch_idx, 'optimizer: ',
+              optimizer.param_groups[0]['lr'])
         epoch_end_time = time()
-        train_writer.add_scalar('Loss/Epoch/cd_pc', avg_cdc, epoch_idx)
-        train_writer.add_scalar('Loss/Epoch/cd_p1', avg_cd1, epoch_idx)
-        train_writer.add_scalar('Loss/Epoch/cd_p2', avg_cd2, epoch_idx)
-        train_writer.add_scalar('Loss/Epoch/cd_p3', avg_cd3, epoch_idx)
-        train_writer.add_scalar('Loss/Epoch/partial_matching', avg_partial, epoch_idx)
+        train_writer.add_scalar('Loss/Epoch/cd_coarse', avg_cdc, epoch_idx)
+        train_writer.add_scalar('Loss/Epoch/cd_fine', avg_cdf, epoch_idx)
+        train_writer.add_scalar('Loss/Epoch/cd_total', avg_cdt, epoch_idx)
         logging.info(
             '[Epoch %d/%d] EpochTime = %.3f (s) Losses = %s' %
-            (epoch_idx, cfg.TRAIN.N_EPOCHS, epoch_end_time - epoch_start_time, ['%.4f' % l for l in [avg_cdc, avg_cd1, avg_cd2, avg_cd3, avg_partial]]))
+            (epoch_idx, cfg.TRAIN.N_EPOCHS, epoch_end_time - epoch_start_time, ['%.4f' % l for l in [avg_cdc, avg_cdf, avg_cdt]]))
 
         # Validate the current model
-        cd_eval = test_net(cfg, epoch_idx, val_data_loader, val_writer, model)
+        cd_eval = test_backbone(
+            cfg, epoch_idx, val_data_loader, val_writer, model)
 
         # Save checkpoints
         if epoch_idx % cfg.TRAIN.SAVE_FREQ == 0 or cd_eval < best_metrics:
-            file_name = 'ckpt-best.pth' if cd_eval < best_metrics else 'ckpt-epoch-%03d.pth' % epoch_idx
+            file_name = 'pcnbackbone-best.pth' if cd_eval < best_metrics else 'ckpt-epoch-%03d.pth' % epoch_idx
             output_path = os.path.join(cfg.DIR.CHECKPOINTS, file_name)
+
+            lr_scheduler_save = lr_scheduler
+            if 'WEIGHTS' not in cfg.CONST:  # train from scratch
+                lr_scheduler_save = lr_scheduler_save.after_scheduler
+
             torch.save({
                 'epoch_index': epoch_idx,
                 'best_metrics': best_metrics,
                 'steps': steps,
                 'model': model.state_dict(),
-                 'optimizer':optimizer.state_dict(),
-                 'lr_scheduler':lr_scheduler.after_scheduler.state_dict()
+                'optimizer': optimizer.state_dict(),
+                'lr_scheduler': lr_scheduler_save.state_dict()
             }, output_path)
 
             logging.info('Saved checkpoint to %s ...' % output_path)
--- main_pcn.py
+++ main_pcn.py
@@ -10,9 +10,7 @@ import sys
 import torch
 from pprint import pprint
 from config_pcn import cfg
-from core.train_pcn import train_net
-from core.test_pcn import test_net
-from core.inference_pcn import inference_net
+
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = cfg.CONST.DEVICE
 
@@ -25,9 +23,14 @@ def set_seed(seed):
 
 
 def get_args_from_command_line():
-    parser = argparse.ArgumentParser(description='The argument parser of SnowflakeNet')
-    parser.add_argument('--test', dest='test', help='Test neural networks', action='store_true')
-    parser.add_argument('--inference', dest='inference', help='Inference for benchmark', action='store_true')
+    parser = argparse.ArgumentParser(
+        description='The argument parser of SnowflakeNet')
+    parser.add_argument('--test', dest='test',
+                        help='Test neural networks', action='store_true')
+    parser.add_argument('--backbone', dest='backbone',
+                        help='First train the PCN as baseline', action='store_true')
+    parser.add_argument('--inference', dest='inference',
+                        help='Inference for benchmark', action='store_true')
     args = parser.parse_args()
 
     return args
@@ -39,23 +42,33 @@ def main():
     print('cuda available ', torch.cuda.is_available())
 
     # Print config
-    print('Use config:')
-    pprint(cfg)
+    # print('Use config:')
+    # pprint(cfg)
 
     if not args.test and not args.inference:
-        train_net(cfg)
+        if args.backbone:
+            from core.train_backbone import train_backbone
+            train_backbone(cfg)
+        else:
+            from core.train_pcn import train_net
+            train_net(cfg)
     else:
         if cfg.CONST.WEIGHTS is None:
-            raise Exception('Please specify the path to checkpoint in the configuration file!')
+            raise Exception(
+                'Please specify the path to checkpoint in the configuration file!')
 
         if args.test:
+            from core.test_pcn import test_net
             test_net(cfg)
         else:
+            from core.inference_pcn import inference_net
             inference_net(cfg)
+
 
 if __name__ == '__main__':
     # Check python version
     seed = 1
     set_seed(seed)
-    logging.basicConfig(format='[%(levelname)s] %(asctime)s %(message)s', level=logging.DEBUG)
+    # logging.basicConfig(
+    #     format='[%(levelname)s] %(asctime)s %(message)s', level=logging.DEBUG)
     main()
